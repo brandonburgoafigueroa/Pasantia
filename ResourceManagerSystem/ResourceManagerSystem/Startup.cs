ae881fd0282a12f6200b3fa54671fdb69e5a5b25
brandon burgoa figueroa <brandonburgoafigueroa@live.com>
2018-03-20 00:00:35 -0400

    requisitos login minimos

--- ResourceManagerSystem/ResourceManagerSystem/Startup.cs
+++ ResourceManagerSystem/ResourceManagerSystem/Startup.cs
@@ -29,9 +29,17 @@ namespace ResourceManagerSystem
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
+            services.AddIdentity<ApplicationUser, IdentityRole>(options => {
+                options.Password.RequiredLength = 6;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireNonAlphanumeric = false;
+            })
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
+                
+ 
 
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
