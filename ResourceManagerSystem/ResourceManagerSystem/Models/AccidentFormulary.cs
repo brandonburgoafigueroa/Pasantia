62ece6a3f867dd20ad7a90d1f5abd208ce9b6445
brandon burgoa figueroa <brandonburgoafigueroa@live.com>
2018-03-20 22:09:29 -0400

    Accident

--- ResourceManagerSystem/ResourceManagerSystem/Models/AccidentFormulary.cs
+++ ResourceManagerSystem/ResourceManagerSystem/Models/AccidentFormulary.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,20 +9,27 @@ namespace ResourceManagerSystem.Models
 {
     public class AccidentFormulary
     {
-        public int NumberOfFormulary { set; get; }
+        public int AccidentFormularyID { set; get; }
+        public string Description { set; get; }
+        public Type TypeOfFormulary { set; get; }
         public DateTime DateOfFullFill { set; get; }
         public string CurrentUser { set; get; }
-        public Type TypeOfFormulary { set; get; }
-        public Employee AffectedEmployee { set; get; }
-        public string Description { set; get; }
-        public ICollection<Injury> Injuries { set; get; }
         public string NatureInjury { set; get; }
         public string ObjectDescription { set; get; }
         public string MedicalDiagnostic { set; get; }
+        //Manualmente select list en la vista
+        [ForeignKey("AffectedEmployee")]
+        public int CIAffectedEmployee { set; get; }
+
+        [ForeignKey("MasterEmployee")]
+        public int CIMasterEmployee { set; get; }
+
+        public Employee AffectedEmployee { set; get; }
         public Employee MasterEmployee { set; get; }
-        public EventLocation EventLocation { set; get; }
-        public AccidentEffect AccidentEffect { set; get; }
-        public ICollection<Person> Signer { set; get; }
+        //public EventLocation EventLocation { set; get; }
+        //public AccidentEffect AccidentEffect { set; get; }
+        ///public ICollection<Injury> Injuries { set; get; }
+        //public ICollection<Person> Signer { set; get; }
 
         public enum Type
         {
