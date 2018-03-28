455519652031abf2631d376d08b975fd96e94dcb
brandon burgoa figueroa <brandonburgoafigueroa@gmail.com>
2018-03-28 19:59:45 -0400

    data anotations

--- ResourceManagerSystem/ResourceManagerSystem/Models/Contrat.cs
+++ ResourceManagerSystem/ResourceManagerSystem/Models/Contrat.cs
@@ -1,19 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ResourceManagerSystem.Models
 {
+    [DisplayName("Contrato")]
     public class Contrat
     {
         [Key]
         public int ContratID { set; get; }
+        [DisplayName("CI")]
         public string CI { set; get; }
+        [DisplayName("Cargo operativo")]
         public int OperativeID { set;get;}
+        [DisplayName("Tipo de contrato")]
         public TypeContrat TypeContrat { set; get; }
+        [DisplayName("Fecha Inicio de contrato")]
         public DateTime DateStart{ set; get; }
+        [DisplayName("Fecha Fin de contrato ")]
         public DateTime DateEnd { set; get; }
 
         public Employee Employee { set; get; }
